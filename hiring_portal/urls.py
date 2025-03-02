5130ca46742b65d7d8757e7e4c02e88fc641733d
inevitablegs <inevitable3101@gmail.com>
2025-03-02 23:37:59 +0530

    UI updated 2nd time

--- hiring_portal/urls.py
+++ hiring_portal/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('candidate/', include('candidates.urls')),
     path('interviewer/', include('interviewers.urls')),
     
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+ 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
